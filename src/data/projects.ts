8c4798c04ba43dcbd9edba7b6325ffbc449a3ef8
YannickSendrey <yannicksendrey@gmail.com>
2023-10-05 17:16:04 +0200

    collapse left
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,8 @@ export type Project = {
   title: string;
   techs: string[];
   link: string;
+  image: string;
+  alt: string;
   isComingSoon?: boolean;
 };
 
@@ -10,16 +12,22 @@ const projects: Project[] = [
     title: " Apotheose Project @O'clock",
     techs: ["React", "Symfony", "..."],
     link: "",
+    image: '',
+    alt: '',
     isComingSoon: true
   },
   {
     title: "PokedexWeb",
     techs: ["React", "Redux", "Laravel", "PostgreSQL", "Docker", "Vite"],
+    image: '/PokedexWeb.jpeg',
+    alt: 'HomePage of PokedexWeb, showing some pokemons',
     link: "https://pokedex-web-yannicksendrey.vercel.app/",
   },
   {
     title: "Mock Reddit Client",
     techs: ["React", "Redux", "Vite"],
+    image: '/MockRedditClient.jpeg',
+    alt: 'homePage of MockRedditClient, show a mock article about React-redux',
     link: "https://mock-reddit-client-yannicksendrey.vercel.app/",
   },
 ];
