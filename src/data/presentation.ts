07fd5f6098dbad76b021bb75611815c472bb77df
YannickSendrey <yannicksendrey@gmail.com>
2023-10-06 19:11:12 +0200

    fix close collapsible only onSectionClick
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -15,7 +15,7 @@ const presentation: Presentation = {
   mail: "yannicksendrey@gmail.com",
   title: "Hi, I’m Yannick 👋",
   description:
-    "Bonjour, i'm a *french fullstack developer* currently looking for an *apprenticeship contract* as soon as January 2024. So far, I am mostly working with *React* (mainly with *Redux*), *Laravel, Symfony*, but I'm also practicing day to day on other technologies such as *TypeScript, NodeJS*",
+    "Bonjour, i'm a *french fullstack developer* currently looking for an *apprenticeship contract* as soon as January 2024. So far, I am mostly working with *React*, *Laravel, Symfony*, but I'm also practicing day to day on other technologies such as *TypeScript, NodeJS*",
   socials: [
     {
       label: "Linkedin",
