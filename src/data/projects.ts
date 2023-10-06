946d31b26b0c153b981b7b33fb513d9aebae8b44
YannickSendrey <yannicksendrey@gmail.com>
2023-10-06 16:09:06 +0200

    added collapse divs with image and description text
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,16 +4,20 @@ export type Project = {
   link: string;
   image: string;
   alt: string;
+  date: string;
+  description: string;
   isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
   {
-    title: " Apotheose Project @O'clock",
+    title: "Apotheose Project @O'clock",
     techs: ["React", "Symfony", "..."],
     link: "",
     image: '',
     alt: '',
+    date: '',
+    description: '',
     isComingSoon: true
   },
   {
@@ -21,6 +25,8 @@ const projects: Project[] = [
     techs: ["React", "Redux", "Laravel", "PostgreSQL", "Docker", "Vite"],
     image: '/PokedexWeb.jpeg',
     alt: 'HomePage of PokedexWeb, showing some pokemons',
+    date: 'September 2023',
+    description: 'Solo-project built in one month (around ~50 hours). *Front-end* displaying a homepage list of pokemons which you can filter by regions deployed on Vercel. A detailed page onClick where you can consult each stat/type of a specific pokemon. *Back-end API* handling a user registration/connection with JWT tokens and a CRUD (add to favorite/remove from favorite). API *dockerized* to deploy it on Render',
     link: "https://pokedex-web-yannicksendrey.vercel.app/",
   },
   {
@@ -28,6 +34,8 @@ const projects: Project[] = [
     techs: ["React", "Redux", "Vite"],
     image: '/MockRedditClient.jpeg',
     alt: 'homePage of MockRedditClient, show a mock article about React-redux',
+    date: 'July-August 2023',
+    description: 'Solo-project built in ~25 hours. Only a Front-end using *React* to display a user-friendly interface where you can up/downvote an article using *Redux* state handling. You can also access to a detailed article page where you can also read comments and up/downvote them. Deployed on Vercel.',
     link: "https://mock-reddit-client-yannicksendrey.vercel.app/",
   },
 ];
